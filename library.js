c50cdc1fcca09aa3d5577cfba3a2f70e3fa4395e
Chimildic <40138097+Chimildic@users.noreply.github.com>
2020-12-18 17:32:21 +0300

    Багфикс: mixin

--- library.js
+++ library.js
@@ -516,7 +516,7 @@ const Combiner = (function () {
                 let yEndIndex = yStartIndex + yRow;
                 push(resultArray, yArray.slice(yStartIndex, yEndIndex));
 
-                if (toLimitOn && !(xArray[(i + 1) * xRow] && yArray[(i + 1) * yRow])) {
+                if (toLimitOn && !(xArray[xEndIndex + xRow] && yArray[yEndIndex + yRow])) {
                     break;
                 }
             }
