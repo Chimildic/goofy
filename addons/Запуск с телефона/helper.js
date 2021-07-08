d28cc6bd4b79cfe486c1e2ff82a31f6719c06d39
Chimildic <40138097+Chimildic@users.noreply.github.com>
2021-07-08 18:36:10 +0300

    Добавлены readState и updateState

--- addons/Запуск с телефона/helper.js
+++ addons/Запуск с телефона/helper.js
@@ -1,13 +1,31 @@
 const Helper = (function () {
+    const FILENAME = 'LaunchState.json';
 
     return {
-      parseId: parseId,
+        parseId: parseId,
+        readState: readState,
+        updateState: updateState,
     };
-  
+
     function parseId(string) {
-      let pattern = '[open.spotify.com|spotify]+[\/|:](track|playlist|album|artist|show|episode|concert|user)[\/|:]([^?/#& ]+)';
-      let [fullMatch, type, id] = string.match(new RegExp(pattern, 'i')) || [];
-      return id;
+        let pattern = '[open.spotify.com|spotify]+[\/|:](track|playlist|album|artist|show|episode|concert|user)[\/|:]([^?/#& ]+)';
+        let [fullMatch, type, id] = string.match(new RegExp(pattern, 'i')) || [];
+        return id;
     }
-  
-  })();
+
+    function readState(id) {
+        return readAllStates()[id] || {};
+    }
+
+    function updateState(id, value = {}) {
+        let states = readAllStates();
+        states[id] = value;
+        Cache.write(FILENAME, states);
+    }
+
+    function readAllStates(){
+        let states = Cache.read(FILENAME);
+        return Array.isArray(states) ? {} : states;
+    }
+
+})();
